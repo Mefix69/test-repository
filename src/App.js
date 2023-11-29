b53cdaedc9864591e01a92c4484c2dc358fb59d8
Mefix69 <102327117+Mefix69@users.noreply.github.com>
2023-11-29 22:04:45 +0300

    feat: add new text

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Ситулиной Надежды Игоревны";
+  const greeting = "Это тестовый проект Ситулиной Надежды Игоревны, new changes";
   // console.log('This is App ');
   return (
     <div className="App">
