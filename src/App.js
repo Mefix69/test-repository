10239e3c2f36d999fc07a6ec768b2b193bc0351c
Mefix69 <102327117+Mefix69@users.noreply.github.com>
2023-11-29 22:05:42 +0300

    chore!:  realise time

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Ситулиной Надежды Игоревны, new changes";
+  const greeting = "Это тестовый проект Ситулиной Надежды Игоревны, new changes, relese";
   // console.log('This is App ');
   return (
     <div className="App">
